262e4ea9baa4506c6319c15ed237d8679e4b0846
vvanslee <vvanslee@gmail.com>
2018-04-06 00:26:43 -0500

    sliding bs modal w/ animate

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    
+    function testAnim(x) {
+        $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
+    };
+    $('#exampleModal').on('show.bs.modal', function (e) {
+      var anim = "slideInRight";
+          testAnim(anim);
+    })
+    $('#exampleModal').on('hide.bs.modal', function (e) {
+      var anim = "slideOutRight";
+          testAnim(anim);
+    })
+
+});
+
+
